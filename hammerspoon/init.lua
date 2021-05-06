3170a69b4a6a274445f2e100ad3c180a0d3f0a91
Kyle Marsh <kyle.l.marsh+github@gmail.com>
2021-05-05 22:33:29 -0700

    [hammerspoon] refresh hammerspoon bindings
    I don't like yabai enough to keep using it, so I'm going back to
    hammerspoon for movement stuff. It looks like there's been quite a lot
    of updates since I last did this, and some of the things I used spoons
    for before are in the core now. I'm halfway through the update -- I have
    focus bindings for directions and switching the monitor and have
    restored my movement bindings (although that still uses the WinWin
    spoon). I want to play around with the grid...I think that might really
    help solidify things

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -1,113 +1,47 @@
 hs.loadSpoon("Caffeine")
 spoon.Caffeine:start()
 
---[[--------------------------------------------------------------------------
+--------------------------------------------------------------------------
 -- Window Movement Bindings
 --------------------------------------------------------------------------
 hs.loadSpoon("WinWin")
+winfocus = {'alt', 'ctrl'}
+winmove = {'cmd', 'alt', 'ctrl'}
+
 local resizebindings = {
-	{key = 'H', position = 'halfleft',  screen = 'left'},
-	{key = 'L', position = 'halfright', screen = 'right'},
-	{key = 'K', position = 'halfup'},
-	{key = 'J', position = 'halfdown'},
+	{key = 'H', position = 'halfleft',  focus = hs.window.filter.focusWest},
+	{key = 'L', position = 'halfright', focus = hs.window.filter.focusEast},
+	{key = 'K', position = 'halfup',    focus = hs.window.filter.focusNorth},
+	{key = 'J', position = 'halfdown',  focus = hs.window.filter.focusSouth},
 	{key = 'Y', position = 'cornerNW'},
 	{key = 'O', position = 'cornerNE'},
 	{key = 'N', position = 'cornerSW'},
 	{key = '.', position = 'cornerSE'},
 	{key = 'U', position = 'fullscreen'},
 }
+
 for _,v in ipairs(resizebindings) do
-	hs.hotkey.bind({'cmd', 'ctrl'}, v.key, function()
+	hs.hotkey.bind(winmove, v.key, function()
 		spoon.WinWin:moveAndResize(v.position)
 	end)
-	if v.screen then
-		hs.hotkey.bind({'cmd', 'alt', 'ctrl'}, v.key, function()
-			spoon.WinWin:moveToScreen(v.screen)
+	if v.focus then
+		hs.hotkey.bind(winfocus, v.key, function()
+            v.focus()
 		end)
 	end
 end
 
-hs.hotkey.bind({'cmd', 'ctrl'}, 'Z', function()
+-- TODO: Explore tweaking this
+hs.hotkey.bind(winfocus, 'g', function()
+    hs.grid.show()
+end)
+
+-- TOOD: can hammerspoon remember the previous location?
+--[[hs.hotkey.bind({'cmd', 'alt', 'ctrl'}, 'Z', function()
 	spoon.WinWin:undo()
-end)
+end)]]
 
-hs.hotkey.bind({'cmd', 'shift'}, 'H', function()
-	windows = hs.window.frontmostWindow():windowsToWest()
-	if windows[1] then
-		windows[1]:focus()
-	end
-end)
-hs.hotkey.bind({'cmd', 'shift'}, 'L', function()
-	windows = hs.window.frontmostWindow():windowsToEast()
-	if windows[1] then
-		windows[1]:focus()
-	end
-end)
-hs.hotkey.bind({'cmd', 'shift'}, 'K', function()
-	windows = hs.window.frontmostWindow():windowsToNorth()
-	if windows[1] then
-		windows[1]:focus()
-	end
-end)
-hs.hotkey.bind({'cmd', 'shift'}, 'J', function()
-	windows = hs.window.frontmostWindow():windowsToSouth()
-	if windows[1] then
-		windows[1]:focus()
-	end
-end)
-]]
---------------------------------------------------------------------------
--- Focus Modal Bindings
---------------------------------------------------------------------------
-focusMode = hs.hotkey.modal.new('cmd', 'e')
-function focusMode:entered()
-	-- Can I change the hammerspoon dock icon instead of alerting
-	--hs.alert'Focus hotkeys'
-end
---[[
-function focusmode:exited()
-	hs.alert'exited mode'
-end
-]]
-
-function focusApp(appName)
-	app = hs.application.find(appName)
-	if not app then
-		hs.alert(appName .. ' not open')
-		focusMode:exit()
-		return
-	end
-	window = app:focusedWindow()
-	window:focus()
-	focusMode:exit()
-end
-
-function focusScreen(direction)
-	curScreen = hs.screen.mainScreen()
-end
-
--- Individual applications
-focusMode:bind('', 'D', function() focusApp('Discord') end)
-focusMode:bind('', 'F', function() focusApp('Firefox') end)
-focusMode:bind('', 'G', function() focusApp('Google Chrome') end)
-focusMode:bind('', 'K', function() focusApp('Keybase') end)
-focusMode:bind('', 'M', function() focusApp('Music') end)
-focusMode:bind('', 'N', function() focusApp('Quiver') end)
-focusMode:bind('', 'Q', function() focusApp('Quiver') end)
-focusMode:bind('', 'S', function() focusApp('Slack') end)
-focusMode:bind('', 'T', function() focusApp('iTerm2') end)
-
--- Other Controls
-focusMode:bind('', 'return', function()
-	hs.window.focusedWindow():toggleFullScreen()
-	focusMode:exit()
-end)
-focusMode:bind('', 'escape', function()
-	hs.alert"Exit focus mode"
-	focusMode:exit()
-end)
-
---[[
+--FIXME: update this to use hs.window.filter(?) and hs.window or something
 -- example code for focusing the next screen:
 -- https://github.com/Hammerspoon/hammerspoon/issues/835
 function focusScreen(screen)
@@ -123,7 +57,77 @@ function isInScreen(screen, win)
 	return win:screen() == screen
 end
 
-hs.hotkey.bind('alt', '\\', function()
-	focusScreen(hs.window.focusedWindow():screen():next())
-end)]]
+hs.hotkey.bind(winfocus, '/', function()
+    screen = hs.screen.mainScreen():next()
+	focusScreen(screen)
+end)
 
+--TODO: figure out key bindings for moving windows between screens
+--[[ move screens
+--hs.hotkey.bind({'cmd', 'shift'}, 'H', function()
+	--windows = hs.window.frontmostWindow():windowsToWest()
+	--if windows[1] then
+		--windows[1]:focus()
+	--end
+--end)
+--hs.hotkey.bind({'cmd', 'shift'}, 'L', function()
+	--windows = hs.window.frontmostWindow():windowsToEast()
+	--if windows[1] then
+		--windows[1]:focus()
+	--end
+--end)
+--hs.hotkey.bind({'cmd', 'shift'}, 'K', function()
+	--windows = hs.window.frontmostWindow():windowsToNorth()
+	--if windows[1] then
+		--windows[1]:focus()
+	--end
+--end)
+--hs.hotkey.bind({'cmd', 'shift'}, 'J', function()
+	--windows = hs.window.frontmostWindow():windowsToSouth()
+	--if windows[1] then
+		--windows[1]:focus()
+	--end
+--end)
+--]]
+
+--------------------------------------------------------------------------
+-- Focus Modal Bindings
+--------------------------------------------------------------------------
+focusMode = hs.hotkey.modal.new('cmd', 'e')
+function focusMode:entered()
+	-- TODO: How can I indicate this other than an "alert"?
+end
+
+function focusApp(appName)
+	app = hs.application.find(appName)
+	if not app then
+		hs.alert(appName .. ' not open')
+		focusMode:exit()
+		return
+	end
+	window = app:focusedWindow()
+	window:focus()
+	focusMode:exit()
+end
+
+-- Individual applications
+focusMode:bind('', 'D', function() focusApp('Discord') end)
+focusMode:bind('', 'F', function() focusApp('Firefox') end)
+focusMode:bind('', 'G', function() focusApp('Google Chrome') end)
+focusMode:bind('', 'K', function() focusApp('Keybase') end)
+focusMode:bind('', 'M', function() focusApp('Music') end)
+focusMode:bind('', 'N', function() focusApp('Quiver') end)
+focusMode:bind('', 'Q', function() focusApp('Quiver') end)
+focusMode:bind('', 'S', function() focusApp('Slack') end)
+focusMode:bind('', 'T', function() focusApp('iTerm2') end)
+focusMode:bind('', 'V', function() focusApp('vivaldi') end)
+
+-- Other Controls
+focusMode:bind('', 'return', function()
+	hs.window.focusedWindow():toggleFullScreen()
+	focusMode:exit()
+end)
+focusMode:bind('', 'escape', function()
+	hs.alert"Exit focus mode"
+	focusMode:exit()
+end)
