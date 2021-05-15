b3608a89a41e1da27cb4baa1627636efab0f2e24
Kyle Marsh <kyle.l.marsh+github@gmail.com>
2021-05-14 22:10:38 -0700

    [hammerspoon] mucked with focusing
    added a slight overlay and a frame that follows focus to help show me
    what window is currently active
    
    made mouse follow focus when shifting focus with the keyboard. This
    includes focusing the most recently-used application on the target
    window when switching windows.

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -1,6 +1,11 @@
 hs.loadSpoon("Caffeine")
 spoon.Caffeine:start()
 
+hs.window.highlight.ui.frameWidth = 5
+hs.window.highlight.ui.frameColor = {0,0.6,1,0.5}
+hs.window.highlight.ui.overlayColor = {0,0,0,0.05}
+hs.window.highlight.ui.overlay = true
+hs.window.highlight.start();
 --------------------------------------------------------------------------
 -- Constants
 --------------------------------------------------------------------------
@@ -16,39 +21,39 @@ end)
 --------------------------------------------------------------------------
 local resizebindings = {
     -- Shift focus and snap focused window to half-screen positions
-	{key = 'H', position = '[0,0,50,100]', focus = hs.window.filter.focusWest},
-	{key = 'L', position = '[50,0,100,100]', focus = hs.window.filter.focusEast},
-	{key = 'K', position = '[0,0,100,50]',    focus = hs.window.filter.focusNorth},
-	{key = 'J', position = '[0,50,100,100]',  focus = hs.window.filter.focusSouth},
+    {key = 'H', position = '[0,0,50,100]', focus = hs.window.filter.focusWest},
+    {key = 'L', position = '[50,0,100,100]', focus = hs.window.filter.focusEast},
+    --TODO instead of focusing north/south maybe use raise/sendToBack on the current screen/space?
+    {key = 'K', position = '[0,0,100,50]',    focus = hs.window.filter.focusNorth},
+    {key = 'J', position = '[0,50,100,100]',  focus = hs.window.filter.focusSouth},
 
     -- Resize focused window on grid
-	{key = 'space', gridFunc = hs.grid.maximizeWindow},
-	{key = '.', gridFunc = hs.grid.snap},
-	{key = '=', gridFunc = hs.grid.resizeWindowWider},
-	{key = '-', gridFunc = hs.grid.resizeWindowThinner},
-	{key = '\\', gridFunc = hs.grid.resizeWindowTaller},
-	{key = '\'', gridFunc = hs.grid.resizeWindowShorter},
+    {key = 'space', gridFunc = hs.grid.maximizeWindow},
+    {key = '.', gridFunc = hs.grid.snap},
+    {key = '=', gridFunc = hs.grid.resizeWindowWider},
+    {key = '-', gridFunc = hs.grid.resizeWindowThinner},
+    {key = '\\', gridFunc = hs.grid.resizeWindowTaller},
+    {key = '\'', gridFunc = hs.grid.resizeWindowShorter},
     -- Move focused window on grid
-	{key = 'Y', gridFunc = hs.grid.pushWindowLeft},
-	{key = 'U', gridFunc = hs.grid.pushWindowDown},
-	{key = 'I', gridFunc = hs.grid.pushWindowUp},
-	{key = 'O', gridFunc = hs.grid.pushWindowRight},
+    {key = 'Y', gridFunc = hs.grid.pushWindowLeft},
+    {key = 'U', gridFunc = hs.grid.pushWindowDown},
+    {key = 'I', gridFunc = hs.grid.pushWindowUp},
+    {key = 'O', gridFunc = hs.grid.pushWindowRight},
 }
 
 for _,v in ipairs(resizebindings) do
-	if v.position then
+    if v.position then
         hs.hotkey.bind(winmove, v.key, function()
             hs.alert(v.position)
             hs.window.focusedWindow():moveToUnit(v.position)
         end)
     end
-	if v.focus then
-		hs.hotkey.bind(winfocus, v.key, function()
+    if v.focus then
+        hs.hotkey.bind(winfocus, v.key, function()
             v.focus()
-            --TODO: maybe also center cursor in new window?
-            -- Or instead, flash the frame, if I can do that?
-		end)
-	end
+            centerMouse()
+        end)
+    end
     if v.gridFunc then
         hs.hotkey.bind(winfocus, v.key, function()
             v.gridFunc(hs.window.focusedWindow())
@@ -65,6 +70,9 @@ if (#hs.screen.allScreens() > 1) then
     -- TODO: maybe instead of explicit screens, loop over screens and check sizes?
     hs.grid.setGrid('3x2', hs.screen.primaryScreen():next())
 end
+--TODO use hs.screen.watcher to detect when a monitor is plugged in or removed
+--and re-set the grid
+
 hs.grid.HINTS={
     {'f9', 'f10', 'f12'}, -- doesn't make sense on my kyria
     {'2', '3', '4'},      --doesn't make sense on my kyria
@@ -76,49 +84,45 @@ hs.hotkey.bind(winfocus, 'c', function()
     hs.grid.show()
 end)
 
---FIXME: update this to use hs.window.filter(?) and hs.window or something
---FIXME: center mouse in focused window, not the whole screen
--- example code for focusing the next screen:
--- https://github.com/Hammerspoon/hammerspoon/issues/835
-function focusScreen(screen)
-	local windows = hs.fnutils.filter(
-		hs.window.orderedWindows(),
-		hs.fnutils.partial(isInScreen, screen))
-	local windowToFocus = #windows > 0 and windows[1] or hs.window.desktop()
-	windowToFocus:focus()
-	local pt = hs.geometry.rectMidPoint(screen:fullFrame())
-	hs.mouse.setAbsolutePosition(pt)
-end
-function isInScreen(screen, win)
-	return win:screen() == screen
-end
-
 hs.hotkey.bind(winfocus, '/', function()
-    screen = hs.screen.mainScreen():next()
-	focusScreen(screen)
+    -- First get the most recently-focused window on the next screen:
+    local wf = hs.window.filter.new()
+    local focusedWindow = hs.window.focusedWindow();
+    local nextScreen = focusedWindow:screen():next()
+    wf:setScreens(nextScreen:id())
+    wf:setSortOrder(hs.window.filter.sortByFocusedLast)
+    local nextWindow = wf:getWindows()[1]
+    -- Then move focus and mouse:
+    nextWindow:focus()
+    centerMouse()
 end)
-
 --TODO: figure out key bindings for moving windows between screens
 -- ehhhh...pushwindowleft/right seems to work.
+
+function centerMouse()
+    window = hs.window.focusedWindow()
+    local windowMidpoint = hs.geometry.rectMidPoint(window:frame())
+    hs.mouse.absolutePosition(windowMidpoint)
+end
 
 --------------------------------------------------------------------------
 -- Focus Modal Bindings
 --------------------------------------------------------------------------
 focusMode = hs.hotkey.modal.new('cmd', 'e')
 function focusMode:entered()
-	-- TODO: How can I indicate this other than an "alert"?
+    -- TODO: How can I indicate this other than an "alert"?
 end
 
 function focusApp(appName)
-	app = hs.application.find(appName)
-	if not app then
-		hs.alert(appName .. ' not open')
-		focusMode:exit()
-		return
-	end
-	window = app:focusedWindow()
-	window:focus()
-	focusMode:exit()
+    app = hs.application.find(appName)
+    if not app then
+        hs.alert(appName .. ' not open')
+        focusMode:exit()
+        return
+    end
+    window = app:focusedWindow()
+    window:focus()
+    focusMode:exit()
 end
 
 -- Individual applications
@@ -135,10 +139,10 @@ focusMode:bind('', 'V', function() focusApp('vivaldi') end)
 
 -- Other Controls
 focusMode:bind('', 'return', function()
-	hs.window.focusedWindow():toggleFullScreen()
-	focusMode:exit()
+    hs.window.focusedWindow():toggleFullScreen()
+    focusMode:exit()
 end)
 focusMode:bind('', 'escape', function()
-	hs.alert"Exit focus mode"
-	focusMode:exit()
+    hs.alert"Exit focus mode"
+    focusMode:exit()
 end)
